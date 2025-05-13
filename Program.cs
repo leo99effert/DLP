339835f2173bf183d45048d94f004b0cae56f8f4
leo99effert <leo99effert@outlook.com>
2025-05-13 16:39:17 +0200

    int_1.0.0 Add basic loop

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿bool isShutdownInitiated = false;
+while (!isShutdownInitiated)
+{
+    Console.WriteLine("DLP is running...");
+    Console.WriteLine("Would you like to exit the application?(y/n)");
+    if (Console.ReadLine()!.ToLower()[0] == 'y')
+    {
+        isShutdownInitiated = true;
+        Console.WriteLine("Shutting down DLP...");
+    }
+}
