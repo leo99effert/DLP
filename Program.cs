a3be702b18abb7de98a1c32623f20da1c8cf94b2
leo99effert <leo99effert@outlook.com>
2025-06-27 20:25:22 +0200

    1.0.7 moved TODO list

--- Program.cs
+++ Program.cs
@@ -12,10 +12,3 @@ IInteraction interaction = new ConsoleInteraction(log, data);
 Application application = new Application(interaction, session);
 await application.Run();
 Console.ReadKey();
-
-// TODO: Remove camelCase from menu actions
-// TODO: Login failed if empty username or with whitespace
-// TODO: Login using ConsoleInteraction not Application
-// TODO: Put methods in right order
-// TODO: Display asyncronously
-// TODO: Move data fetching from ConsoleInteraction to Application
